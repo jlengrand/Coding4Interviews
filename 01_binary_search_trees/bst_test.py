a6264998bce297f69d6389745f4b21a0366fa55f
Julien Lengrand-Lambert <jlengrand@gmail.com>
2013-07-07 11:27:40 +0200

    First test passes

--- 01_binary_search_trees/bst_test.py
+++ 01_binary_search_trees/bst_test.py
@@ -3,3 +3,19 @@ Unit tests for the binary search tree data structure
 @jlengrand
 2013/07
 """
+
+from bst import BinarySearchTree
+
+import unittest
+
+
+class test_binary_search_tree(unittest.TestCase):
+
+    def test_add(self):
+
+        bst = BinarySearchTree()
+        self.assertEqual(bst.size, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
